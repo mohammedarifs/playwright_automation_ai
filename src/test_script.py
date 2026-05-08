fa2afe6a1e4b06189b085198b31c1abd8ae83ad3
Mohammed Arif Shaik <arifshaik321@gmail.com>
2026-05-08 09:41:11 +0530

    adding code for playwright

--- src/test_script.py
+++ src/test_script.py
@@ -1,1 +1,12 @@
-print("sssss")
+from playwright.sync_api import sync_playwright
+
+
+
+
+def test_scripting():
+    with sync_playwright() as p:
+        browser = p.webkit.launch()
+        page = browser.new_page()
+        page.goto("https://playwright.dev/")
+        page.screenshot(path="./resource/example.png")
+        browser.close()
